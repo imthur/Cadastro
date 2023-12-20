71ed21e2e9fd39168f593f051cde0d3990125cbc
Arthur Aguiar <101827286+imthur@users.noreply.github.com>
2023-12-20 10:50:43 -0300

    Update models.py
    Adicionar função para tratamento de duplicidade de dados.

--- projeto_cadUser/app_cadUser/models.py
+++ projeto_cadUser/app_cadUser/models.py
@@ -5,3 +5,8 @@ class Usuario(models.Model):
     idUser = models.AutoField(primary_key=True)
     nome = models.TextField(max_length=255)
     idade = models.IntegerField()
+    def save(self, *args, **kwargs):
+        if Usuario.objects.filter(nome=self.nome, idade=self.idade).exists():
+            pass
+        else:
+            super(Usuario, self).save(*args, **kwargs)
